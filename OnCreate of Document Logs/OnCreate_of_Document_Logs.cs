6fd42ab30ba5042a88b5d1a46f49b08d6aeef37e
Joao Frade <joao.frade97@gmail.com>
2024-02-07 11:53:28 +0000

    final

--- OnCreate of Document Logs/OnCreate_of_Document_Logs.cs
+++ OnCreate of Document Logs/OnCreate_of_Document_Logs.cs
@@ -6,6 +6,7 @@ using System.Runtime.Remoting.Contexts;
 using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 
@@ -34,9 +35,9 @@ namespace OnCreate_of_Document_Logs
                 case "CREATE":
                     OnCreate();
                     break;
-                //case "UPDATE":
-                //    OnUpdate();
-                //    break;
+                case "UPDATE":
+                    OnUpdate();
+                    break;
                 //case "DELETE":
                 //    OnDelete();
                 //    break;
@@ -47,17 +48,19 @@ namespace OnCreate_of_Document_Logs
 
         private void OnCreate()
         {
-            
+            trace.Trace("update");
+
             if (_context.InputParameters.Contains("Target") && _context.InputParameters["Target"] is Entity)
             {
                 // Get Record
                 Entity createdDocumentLog = (Entity)_context.InputParameters["Target"];
 
-                
+
                 if (createdDocumentLog.LogicalName == "arq_documentlog")
                 {
                     // New Record
-                    Entity createdDocumentPartyLog = new Entity("arq_documentpartylog");
+                    Entity createdDocumentPartyLogFROM = new Entity("arq_documentpartylog");
+                    Entity createdDocumentPartyLogTO = new Entity("arq_documentpartylog");
                     String username = "";
                     var number = 0;
                     var stringnumber = "";
@@ -79,7 +82,7 @@ namespace OnCreate_of_Document_Logs
                         {
                             //  "Internal";
                             username = "Internal";
-                            prefixcode = "INT"+last2digitsofyear+"-";
+                            prefixcode = "INT" + last2digitsofyear + "-";
 
 
                         }
@@ -87,7 +90,7 @@ namespace OnCreate_of_Document_Logs
                         {
                             // Inbound";
                             username = "External";
-                            prefixcode = "INB" + last2digitsofyear + "-" ;
+                            prefixcode = "INB" + last2digitsofyear + "-";
 
                         }
                         else if (doctype == 3)
@@ -96,191 +99,224 @@ namespace OnCreate_of_Document_Logs
                             username = "External";
                             prefixcode = "OUT" + last2digitsofyear + "-";
                         }
-                        else                        
-                        {
-
-                        }
-
-
-                        // Instantiate QueryExpression query
-                        var query = new QueryExpression("arq_documentpartylog");
-                        query.TopCount = 1;
-                        // Add all columns to query.ColumnSet
-                        query.ColumnSet.AllColumns = true;
-                        
-                        // Add orders
-                        query.AddOrder("createdon", OrderType.Descending);
-
-
-                        EntityCollection results = service.RetrieveMultiple(query);
-
-                        if (results.Entities.Count > 0)
-                        {
-                            
-                            Entity firstRecord = results.Entities[0];  
-                                                      
-                            var arqNameValue = firstRecord["arq_name"].ToString();
-                            stringnumber = arqNameValue.Substring(6, 6);
-                            var intnumber = int.Parse(stringnumber);
-                            number = intnumber + 1;
-
-                        }
                         else
                         {
-                            number = 1;
-                        }
 
-                       
-                        createdDocumentPartyLog["arq_name"] = $"DPL{last2digitsofyear}-{number.ToString("000000")}-{username}";
+                        }
+                        
+                        
+                        
+
+                        createdDocumentPartyLogFROM["arq_document"] = new EntityReference("arq_documentlog", createdDocumentLog.Id);
+                        createdDocumentPartyLogTO["arq_document"] = new EntityReference("arq_documentlog", createdDocumentLog.Id);
 
                     }
 
-                    Guid newRecordId = service.Create(createdDocumentPartyLog);
+                    Guid newRecordIdFROM = service.Create(createdDocumentPartyLogFROM);
+                    Guid newRecordIdTO = service.Create(createdDocumentPartyLogTO);
+
+                    
+                    ColumnSet columns2 = new ColumnSet("arq_code");
+                    
+                    Entity retrievedRecordfrom = service.Retrieve("arq_documentpartylog", newRecordIdFROM, columns2);
+                    var codefrom = retrievedRecordfrom.GetAttributeValue<String>("arq_code");
+                    var x = codefrom.Substring(6, codefrom.Length - 6);
+
+                    retrievedRecordfrom["arq_name"] = $"DPL{last2digitsofyear}-{x}-From";
+                    retrievedRecordfrom["arq_code"] = $"DPL{last2digitsofyear}-{x}";
+
+                    service.Update(retrievedRecordfrom);
 
 
-                    createdDocumentLog["arq_docfromparty"] = new EntityReference("arq_documentlog", newRecordId);
-                    createdDocumentLog["arq_doctoparty"] = new EntityReference("arq_documentlog", newRecordId);
-                    createdDocumentLog["arq_name"] = "Document Log: " + createdDocumentLog.GetAttributeValue<string>("arq_subject");
+                    Entity retrievedRecordto = service.Retrieve("arq_documentpartylog", newRecordIdTO, columns2);
+                    var codeto = retrievedRecordto.GetAttributeValue<String>("arq_code");
+                    var y = codeto.Substring(6, codeto.Length - 6);
+
+                    retrievedRecordto["arq_name"] = $"DPL{last2digitsofyear}-{y}-To";
+                    retrievedRecordto["arq_code"] = $"DPL{last2digitsofyear}-{y}";
+
+
+                    service.Update(retrievedRecordto);
+
+
+
+
+
+
+
+
+
+
+                    createdDocumentPartyLogTO["arq_name"] = $"name";
+
+
+
+
+
+
+
+                    Entity myentity = service.Retrieve(createdDocumentLog.LogicalName, createdDocumentLog.Id, new ColumnSet("arq_name"));
+
+
+
+                    myentity["arq_docfromparty"] = new EntityReference("arq_documentlog", newRecordIdFROM);
+                    myentity["arq_doctoparty"] = new EntityReference("arq_documentlog", newRecordIdTO);
 
                     var code = createdDocumentLog.GetAttributeValue<string>("arq_code").ToString();
-                    createdDocumentLog["arq_code"] = prefixcode + code;
-                    service.Update(createdDocumentLog);
+                    myentity["arq_code"] = prefixcode + code;
+                    myentity["arq_name"] = prefixcode + code + "-" + createdDocumentLog.GetAttributeValue<string>("arq_subject");
+                    service.Update(myentity);
 
 
-                    trace.Trace("Novo registro criado na entidade destino com ID: " + newRecordId);
+                    //trace.Trace("Novo registro criado na entidade destino com ID: " + newRecordId);
                 }
             }
         }
 
+        private void OnUpdate()
+        {
+            trace.Trace("update");
+            var username = "";
+            var number = 0;
+            var stringnumber = "";
+            var prefixcode = "";
+            string currentyear = DateTime.Now.Year.ToString();
+            string last2digitsofyear = currentyear.Substring(currentyear.Length - 2);
 
 
-        //private void OnUpdate()
-        //{
-        //    //trace.Trace("Entered the Update function.");
-        //    if (_context.InputParameters.Contains("Target") && _context.InputParameters["Target"] is Entity)
-        //    {
-        //        //validations required to end loop
-        //        //booking can't be status: Travel Time or Canceled
-        //        Entity myEntity = (Entity)_context.InputParameters["Target"];
-        //        Entity booking = service.Retrieve("bookableresourcebooking", myEntity.Id, new ColumnSet("bookingstatus", "ipg_originalstarttime", "ipg_traveltime", "starttime"));
+            if (_context.InputParameters.Contains("Target") && _context.InputParameters["Target"] is Entity)
+            {
 
-        //        DateTime startTime = new DateTime();
-        //        DateTime originalStartTime = new DateTime();
-        //        int travelTime = 0;
+                trace.Trace("if");
 
-        //        //startime value validation
-        //        if (myEntity.Contains("starttime"))
-        //        {
-        //            startTime = myEntity.GetAttributeValue<DateTime>("starttime");
-        //            //trace.Trace("Trigger contains starttime: " + startTime.ToString());
-        //        }
-        //        else
-        //        {
-        //            startTime = booking.GetAttributeValue<DateTime>("starttime");
-        //            //trace.Trace("Trigger does not contain starttime. Getting from record: " + startTime.ToString());
-        //        }
+                // Get Record
 
-        //        //traveltime value validation
-        //        if (myEntity.Contains("ipg_traveltime"))
-        //        {
-        //            travelTime = myEntity.GetAttributeValue<int>("ipg_traveltime");
-        //            //trace.Trace("Trigger contains ipg_traveltime: " + travelTime.ToString());
-        //        }
-        //        else
-        //        {
-        //            travelTime = booking.GetAttributeValue<int>("ipg_traveltime");
-        //            //trace.Trace("Trigger does not contain ipg_traveltime. Getting from record: " + travelTime.ToString());
-        //        }
+                Entity updatedDocumentLog = (Entity)_context.InputParameters["Target"];
 
-        //        //originalStartTime value validation
-        //        if (booking.Contains("ipg_originalstarttime"))
-        //        {
-        //            originalStartTime = booking.GetAttributeValue<DateTime>("ipg_originalstarttime");
-        //            //trace.Trace("Original Start Time: " + originalStartTime);
-        //        }
-        //        else
-        //        {
-        //            //trace.Trace("ipg_originalstarttime is null on the record.");
-        //        }
-        //        //check BookingStatus
-        //        if (booking.GetAttributeValue<EntityReference>("bookingstatus").Name != "Travel Time" //Travel Time
-        //            && booking.GetAttributeValue<EntityReference>("bookingstatus").Name != "Canceled") //Canceled  
-        //        {
-        //            //trace.Trace("Booking not Travel Time or Canceled, current startTime: " + startTime.ToString());
 
-        //            //calculations verification
-        //            //trace.Trace("Subtraction between start and travel: " + startTime.AddMinutes(-travelTime).ToString());
-
-        //            //originalstartttime null?
-        //            if (originalStartTime != startTime.AddMinutes(-travelTime))
-        //            {
-        //                //trace.Trace("All Validations sucessful!");
-
-        //                //check information received in the trigger
-        //                if (myEntity.Contains("starttime") && myEntity.Contains("ipg_traveltime"))
-        //                {
-        //                    //trace.Trace("Trigger contained starttime: " + startTime.ToString() + " and travelTime: " + travelTime.ToString());
-        //                }
-        //                else if (myEntity.Contains("ipg_traveltime"))
-        //                {
-        //                    startTime = originalStartTime;
-        //                    //trace.Trace("Trigger contains ipg_traveltime: " + travelTime.ToString());
-        //                }
-        //                UpdateBookingOrTravel(myEntity, startTime, travelTime);
-        //            }
-        //            else
-        //            {
-        //                //trace.Trace("There is no difference between Original Start Time and the subtraction between start and travel.");
-        //            }
-        //        }
-        //        else
-        //        {
-        //            //trace.Trace("Booking was Travel Time or Canceled.");
-        //        }
-        //    }
-        //}
-
-        //private void OnDelete()
-        //{
-        //    //trace.Trace("Entered the Delete function."); 
-        //    var image = _context.PreEntityImages.FirstOrDefault().Value;
-        //    //trace.Trace("Obtained the image: " + image.Id.ToString());
-        //    if (image.Contains("ipg_relatedbooking"))
-        //    {
-        //        EntityReference relatedBooking = image.GetAttributeValue<EntityReference>("ipg_relatedbooking");
-        //        onDeleteTravelResetBooking(relatedBooking);
-        //    }
-        //    else
-        //    {
-        //        //trace.Trace("Related Booking was empty");
-
-        //        //not needed currently, as there is already a PA flow doing this.
-        //        //get all related child bookings, and also delete
-        //        //EntityCollection RelatedBookings = GetRelatedBookings(image.Id);
-        //        //trace.Trace("Related Booking obtained: " + RelatedBookings.Entities.Count().ToString());
-        //        //if (RelatedBookings.Entities.Count() > 0)
-        //        //{
-        //        //    foreach (Entity x in RelatedBookings.Entities)
-        //        //    {
-        //        //        trace.Trace("Related Booking to Delete: " + x.Id.ToString());
-        //        //        service.Delete(x.LogicalName, x.Id);
-        //        //    }
-        //        //}
-        //    }
-        //}
-
-        //private void onCreateSetOriginal(Entity trigger)
-        //{
-        //    //trace.Trace("Entered the onCreateSetOriginal function.");
-
-        //    Entity myBookingRecord = service.Retrieve("bookableresourcebooking", trigger.Id, new ColumnSet("starttime"));
-        //    DateTime startTime = myBookingRecord.GetAttributeValue<DateTime>("starttime");
-        //    myBookingRecord["ipg_originalstarttime"] = startTime;
-        //    service.Update(myBookingRecord);
-        //    //trace.Trace("Booking record updated.");
-        //}
+                if (updatedDocumentLog.LogicalName == "arq_documentlog")
+                {
+                    //New Record
+                    trace.Trace("1");
 
 
 
+
+
+                    trace.Trace("2");
+
+
+                    EntityReference relatedEntityReference = updatedDocumentLog.GetAttributeValue<EntityReference>("arq_doctype");
+
+
+                    if (relatedEntityReference != null )
+                    {
+                        trace.Trace("3");
+                        Guid doctypeid = relatedEntityReference.Id;
+                        ColumnSet columns = new ColumnSet("arq_documentclassification");
+                        Entity docRecord = service.Retrieve("arq_documenttype", doctypeid, columns);
+                        OptionSetValue optionSetValue = docRecord.GetAttributeValue<OptionSetValue>("arq_documentclassification");
+                        int doctype = optionSetValue.Value;
+                        trace.Trace("4");
+
+
+                        if (doctype == 1)
+                        {
+                            //  "Internal";
+                            username = "Internal";
+                            prefixcode = "INT" + last2digitsofyear + "-";
+
+
+                        }
+                        else if (doctype == 2)
+                        {
+                            // Inbound";
+                            username = "External";
+                            prefixcode = "INB" + last2digitsofyear + "-";
+
+                        }
+                        else if (doctype == 3)
+                        {
+                            // Outbound";
+                            username = "External";
+                            prefixcode = "OUT" + last2digitsofyear + "-";
+                        }
+                        else
+                        {
+
+                        }
+
+                        trace.Trace("5");
+                        Entity myentity = service.Retrieve(updatedDocumentLog.LogicalName, updatedDocumentLog.Id, new ColumnSet("arq_name"));
+                        trace.Trace("6");
+
+                        var prename = myentity.GetAttributeValue<string>("arq_name").ToString();
+                        trace.Trace("7");
+
+                        String[] vector = prename.Split('-');
+                        Array.Reverse(vector);
+                        Array.Resize(ref vector, vector.Length - 1);
+                        Array.Reverse(vector);
+                        string result = string.Join("-", vector);
+                        myentity["arq_name"] = prefixcode+result;
+
+
+                        Array.Resize(ref vector, vector.Length - 1);
+                        string resultcode = string.Join("-", vector);
+
+                        myentity["arq_code"] = prefixcode + resultcode;
+
+
+
+                        
+
+                        service.Update(myentity);
+
+
+                    }
+
+                    trace.Trace("3");
+                    String relatedEntityReferenceSubject = updatedDocumentLog.GetAttributeValue<String>("arq_subject");
+
+
+                    if (relatedEntityReferenceSubject != null)
+                    {
+                        trace.Trace("subject");
+                        Entity myentity = service.Retrieve(updatedDocumentLog.LogicalName, updatedDocumentLog.Id, new ColumnSet("arq_name", "arq_subject"));
+                        trace.Trace("1");
+                        var prename = myentity.GetAttributeValue<string>("arq_name").ToString();
+                        trace.Trace("2");
+                        var subj = myentity.GetAttributeValue<string>("arq_subject").ToString();
+                        trace.Trace("3");
+                        String[] vector = prename.Split('-');
+                        Array.Resize(ref vector, vector.Length - 1);
+                        string result = string.Join("-", vector);
+
+                        trace.Trace("4");
+                        myentity["arq_name"] = result +"-"+ subj;
+                        trace.Trace("5");
+
+                        if (_context.Depth == 1)
+                        {
+                            // Atualiza o registro apenas se a profundidade for 1 (não foi chamado novamente pelo próprio plugin)
+                            service.Update(myentity);
+                        }
+
+
+                    }
+
+
+
+
+                }
+
+                trace.Trace("else");
+            }
+
+
+
+
+
+        }
     }
 }
